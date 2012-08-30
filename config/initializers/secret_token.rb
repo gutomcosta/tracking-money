c7770f9e7fa3c3147c913fa9a9ea9c2cc77b4f41
Luiz Costa <gutomcosta@gmail.com>
2012-08-29 23:13:42 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackingMoney::Application.config.secret_token = '01094e75cecee8f06e0a6bb38fdba1cc130ae42c0e83a7fe9f3050bf3c8b3757087e8889315d421bc17e5c5d40c4d99c02ac3f28a1e77a7e356c7ea1574f0383'
